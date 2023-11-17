529c9a9bee738dea9604258e3ccb7d788839d696
Faklor <faklor_br_ar@mail.ru>
2023-11-17 03:36:43 +0300

    prod

---
+++ mobile/src/App.js
@@ -0,0 +1,25 @@
+import './App.css'
+import DoubleButton from './DoubleButton'
+import hoc from './withRainbowFrame'
+
+
+
+function App() {
+
+  let colors = ['red','orange', 'yellow','green', '#00BFFF', 'blue', 'purple']
+
+  let FramedDoubleButton=hoc(DoubleButton)
+
+  return (
+    <div className="App">
+      <DoubleButton caption1 ={'однажды'} caption2={'пору'} cbPressed={num=>{return alert(num)} }>
+        в студеную зимнюю
+      </DoubleButton>
+      <FramedDoubleButton caption1="я из лесу" caption2="мороз" cbPressed={ num => alert(num) } colors={colors}>
+        вышел, был сильный
+      </FramedDoubleButton>
+    </div>
+  );
+}
+
+export default App
