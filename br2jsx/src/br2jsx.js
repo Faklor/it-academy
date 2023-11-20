ddc17c576b07d5974c70a10a854b87b5a90b29a0
Faklor <faklor_br_ar@mail.ru>
2023-11-20 12:59:41 +0300

    br2jsx

--- br2jsx/src/br2jsx.js
+++ br2jsx/src/br2jsx.js
@@ -6,17 +6,23 @@ export default class br2jsx extends Component{
 
     render(props){
         const a = []
-        this.props.text.replace(/(&lt;|<)br\s*\/?(&gt;|>)/g, ',').split(',').map(i=>{
-            a.push(i)
-            if(i){
-                a.push(<br/>)    
+        // this.props.text.replace(/(&lt;|<)br\s*\/?(&gt;|>)/g, ',').split(',').map(i=>{
+        //     a.push(i)
+        //     if(i){
+        //         a.push(<br/>)    
+        //     }
+        //     // return  <>
+        //     //     {i}
+        //     //     <br/>
+        //     // </>
+        // })
+        this.props.text.split(/<br *\/?>/).forEach(element => {
+            a.push(element)
+            if(element){
+                a.push(<br/>)
             }
-            // return  <>
-            //     {i}
-            //     <br/>
-            // </>
-        })
-
+        });
+        
 
         return(
             <>
--- rainbowframehoc/src/withRainbowFrame.js
+++ rainbowframehoc/src/withRainbowFrame.js
@@ -1,10 +1,10 @@
 const {Component} = require('react')
 
-const Hoc = (colors)=> (NewCompanent)=>{
+const Hoc = (colors)=> (Companent)=>{
     return class HOC extends Component{
      
         render(){
-            let a = <NewCompanent {...this.props}/>
+            let a = <Companent {...this.props}/>
            colors.forEach(i=>{ 
                 a = <div style={{border:`2px solid ${i}`}}>
                     {a}
